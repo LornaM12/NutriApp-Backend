3a50d23c9283330d932562eb93e63c7f8e10ab14
Lorna Munanie <lornamunanie32@gmail.com>
2025-08-26 14:29:55 +0300

    added new cors

--- recommender/api.py
+++ recommender/api.py
@@ -153,6 +153,7 @@ origins = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",  # running port
     "https://nutrition-app-pndw.vercel.app",  # Deployed frontend URL
+    "https://nutrition-app-pi.vercel.app"
     "http://localhost:63342",
     "http://127.0.0.1:8001",
     "http://localhost:8001",
